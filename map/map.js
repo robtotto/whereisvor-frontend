d48d4a94d78f923da521fc785f47ef3bb28fbc84
Bogdan2810 <121561815+Bogdan2810@users.noreply.github.com>
2024-02-06 18:15:09 +0200

    changed marker placing on map

--- map/map.js
+++ map/map.js
@@ -1,33 +1,42 @@
 // Dummy data
 const coords = [
-  [25.619098069991736, 45.64838769062287],
-  [25.584508303390475, 45.646107614795504],
-  [25.630170228531597, 45.664705523692845],
-  [25.585881594372164, 45.662066196611036],
+  [25.619098, 45.648387],
+  [25.584508, 45.646107],
+  [25.63017, 45.664705],
+  [25.585881, 45.662066],
 ];
 
+// Add map:
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicmFkdWZpbGkiLCJhIjoiY2xzNGp2MTN5MWVldTJqb2UzbDVhNWhobyJ9.JyqZzFSgW4xiWNn6nwXkXw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [26.366689, 46.930792], // starting position [lng, lat]
+  zoom: 14, // starting zoom
+});
+
+// Add markers to map
+coords.map(coord => {
+  const marker = new mapboxgl.Marker().setLngLat([coord[0], coord[1]]).addTo(map);
+});
+
+// Check geolocation and set map center if ok
 if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(position => {
     const [lng, lat] = [position.coords.longitude, position.coords.latitude];
     map.setCenter([lng, lat]);
-    coords.map(coord => {
-      const marker = new mapboxgl.Marker().setLngLat([coord[0], coord[1]]).addTo(map);
-    });
   });
 } else {
   alert('Geolocation is not supported by your browser');
 }
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYm9nZGFuLTI4IiwiYSI6ImNsczRrNDBjdzA0YTYycm54ZWd2M212Z2oifQ.baXXtWrFcumCCbeXt7E6dw';
+// console.log(
+//   decodeURIComponent(
+//     'https://api.mapbox.com/directions/v5/mapbox/walking/-74.173431%2C40.77917%3B-74.175859%2C40.762212?access_token=pk.eyJ1IjoiYm9nZGFuLTI4IiwiYSI6ImNsczNobDdicDB5cWcydm1lOGtnMXZjYWkifQ.UI-Umu7Pb1hHE2ZsQ7DYBQ'
+//   )
+// );
 
-// GT token
 /*
-pk.eyJ1IjoicmFkdWZpbGkiLCJhIjoiY2xzNGp2MTN5MWVldTJqb2UzbDVhNWhobyJ9.JyqZzFSgW4xiWNn6nwXkXw
+https://api.mapbox.com/directions/v5/mapbox/walking/-74.173431,40.77917;-74.175859,40.762212?alternatives=false&geometries=geojson&overview=simplified&steps=false&access_token=pk.eyJ1IjoiYm9nZGFuLTI4IiwiYSI6ImNsczNobDdicDB5cWcydm1lOGtnMXZjYWkifQ.UI-Umu7Pb1hHE2ZsQ7DYBQ
 */
-
-const map = new mapboxgl.Map({
-  container: 'map',
-  center: [26.366689, 46.930792], // starting position [lng, lat]
-  zoom: 15, // starting zoom
-});
