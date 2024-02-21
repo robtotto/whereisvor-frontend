3efc58dbfaec82a5a48a1b209374c4db680f9117
Monica Laura Burns <58688612+monicalaura@users.noreply.github.com>
2024-02-21 17:44:32 +0200

    Fixed errors in slick.js

--- public/js/slick.js
+++ public/js/slick.js
@@ -1,28 +1,29 @@
 $(document).ready(function () {
-            $('.partnersCarousel').slick({
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                autoplay: true,
-                autoplaySpeed: 2000,
-                arrows: false,
-                dots: false,
-                pauseOnHover: true,
-                responsive: [
-                    {
-                        breakpoint: 768,
-                        settings: {
-                            slidesToShow: 2
-                        }
-                    },
-                    {
-                        breakpoint: 520,
-                        settings: {
-                            slidesToShow: 2
-                        }
-                    }
-                ]
-            });
-        });
+    $('.partners-carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+});
+
 
 $(document).ready(function(){
 $('.carousel-GT').slick({
