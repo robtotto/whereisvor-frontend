435f942342f59565268d82a8227293e82ccfa582
Monica Laura Burns <58688612+monicalaura@users.noreply.github.com>
2024-02-21 09:39:22 +0200

    Added error handling and reverse geocoding to list.js

--- public/js/list.js
+++ public/js/list.js
@@ -1,11 +1,8 @@
 // list.js
+import { getSprings, displayErrorPopup } from './lib/utils.js';
 
-import {getSprings } from './lib/utils.js';
-
-// fetching springs 
-const springs = await getSprings();
-// console.log(springs);
- 
+// Fetching springs data and proceeding
+getSprings().then(springs => {
     const gridCards = document.querySelector('.grid-cards');
     if (springs) {
         springs.forEach(item => {
@@ -17,42 +14,67 @@ const springs = await getSprings();
         errorMessage.textContent = 'Nu s-au putut încărca datele.';
         gridCards.appendChild(errorMessage);
     }
+});
 
 function createCard(item) {
-  const card = document.createElement('div');
-  card.classList.add('overflow-hidden', 'border-secondary', 'card', 'card-details');
+    const card = document.createElement('div');
+    card.classList.add('overflow-hidden', 'border-secondary', 'card', 'card-details');
 
-  const img = document.createElement('img');
-  img.src = item.image;
-  img.classList.add('img-spring-card');
-  card.appendChild(img);
+    const img = document.createElement('img');
+    img.src = item.image;
+    img.classList.add('img-spring-card');
+    card.appendChild(img);
 
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
-  card.appendChild(cardBody);
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    card.appendChild(cardBody);
 
-  const title = document.createElement('h5');
-  title.classList.add('card-title');
-  title.innerHTML = `<b>${item.name}</b>`;
-  cardBody.appendChild(title);
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.innerHTML = `<b>${item.name}</b>`;
+    cardBody.appendChild(title);
 
-  const locationLabel = document.createElement('p');
-  locationLabel.classList.add('card-text');
-  locationLabel.textContent = 'Locație:';
-  cardBody.appendChild(locationLabel);
+    const locationLabel = document.createElement('p');
+    locationLabel.classList.add('card-text');
+    locationLabel.textContent = 'Locație:';
+    cardBody.appendChild(locationLabel);
 
-  const location = document.createElement('p');
-  location.classList.add('card-text-km');
-  location.textContent = `Latitudine: ${item.latitude}, Longitudine: ${item.longitude}`;
-  cardBody.appendChild(location);
+    const location = document.createElement('p');
+    location.classList.add('card-text-km');
+    cardBody.appendChild(location);
 
-  const heartIcon = document.createElement('img');
-  heartIcon.src = 'Images/heart-symbol.svg';
-  heartIcon.classList.add('position-absolute', 'bottom-0', 'end-0', 'heart-card');
-  cardBody.appendChild(heartIcon);
+    const heartIcon = document.createElement('img');
+    heartIcon.src = 'Images/heart-symbol.svg';
+    heartIcon.classList.add('position-absolute', 'bottom-0', 'end-0', 'heart-card');
+    cardBody.appendChild(heartIcon);
 
-  return card;
+    // Reverse geocoding spring's location
+    reverseGeocode(item.longitude, item.latitude)
+    .then(springAddress => {
+        const location = card.querySelector('.card-text-km');
+        location.textContent = `${springAddress}`;
+    })
+    .catch(error => {
+        // Custom error handling
+        displayErrorPopup(`Location not found: ${error}`);
+    });
+
+    return card;
 }
 
-// Apelăm funcția pentru afișarea datelor când se încarcă pagina
-window.addEventListener('DOMContentLoaded', displaysprings);
+const reverseGeocode = async (lng, lat) => {
+    const accessToken = 'pk.eyJ1IjoicmFkdWZpbGkiLCJhIjoiY2xzNGp2MTN5MWVldTJqb2UzbDVhNWhobyJ9.JyqZzFSgW4xiWNn6nwXkXw';
+    try {
+        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${accessToken}`);
+        const data = await response.json();
+        if (data.features && data.features.length > 0) {
+            return data.features[0].place_name;
+        } else {
+            throw new Error('Address not found');
+        }
+    } catch (error) {
+        // Custom error handling
+        displayErrorPopup(`Location not found: ${error}`);
+        throw error; 
+    }
+};
